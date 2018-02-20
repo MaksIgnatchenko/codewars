f4e6de533cdb97b7673dff9ff0c8e3cf11beb400
Maks Ignatchenko <foecunditate@gmail.com>
2018-02-20 09:34:55 +0200

    before checking

--- JAN.php
+++ JAN.php
@@ -8,4 +8,14 @@ function ann($n){
     return $done;
 }
 
-print_r(ann(6));
+function john($n){
+    $doneA[0] = 1;
+    $doneJ[0] = 0;
+    for ($i = 1; $i <= $n; $i++){
+        $doneJ[$i] = $i - $doneA[$doneJ{$i - 1}];
+        $doneA[$i] = $i - $doneJ[$doneA{$i - 1}];
+    }
+    return $doneJ;
+}
+
+print_r(john(6));
