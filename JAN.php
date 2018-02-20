84a1ba1093959c2bfe998f915e7f05773567da3a
Maks Ignatchenko <foecunditate@gmail.com>
2018-02-20 10:05:47 +0200

    Ann and John working

--- JAN.php
+++ JAN.php
@@ -1,4 +1,45 @@
 <?php
+/*
+ Description:
+John and his wife Ann have decided to go to Codewars.
+
+On day 0 Ann will do one kata and John - he wants to know how it is working - 0.
+
+Let us call a(n) the number of katas done by Ann at day n we have a(0) = 1 and in the same manner j(0) = 0.
+
+They have chosen the following rules:
+
+On day n the number of katas done by Ann should be n minus the number of katas done by John at day t, t being equal to the number of katas done by Ann herself at day n - 1.
+
+On day n the number of katas done by John should be n minus the number of katas done by Ann at day t, t being equal to the number of katas done by John himself at day n - 1.
+
+Whoops! I think they need to lay out a little clearer exactly what there're getting themselves into!
+
+Could you write:
+1) two functions ann and john (parameter n) giving the list of the numbers of katas Ann and John should take on each day from day 0 to day n - 1 (n days - see first example below)?
+2) The total number of katas taken by ann (function sum_ann(n)) and john (function sum_john(n)) from day 0 (inclusive) to day n (exclusive)?
+Examples:
+john(11) -->  [0, 0, 1, 2, 2, 3, 4, 4, 5, 6, 6]
+ann(6) -->  [1, 1, 2, 2, 3, 3]
+
+sum_john(75) -->  1720
+sum_ann(150) -->  6930
+Shell Note:
+sumJohnAndAnn has two parameters:
+
+first one : n (number of days, $1)
+
+second one : which($2) ->
+
+1 for getting John's sum
+
+2 for getting Ann's sum.
+
+See "Sample Tests".
+
+Note:
+Keep an eye on performance.
+ */
 function john($n){
     $doneA[0] = 1;
     $doneJ[0] = 0;
@@ -19,6 +60,12 @@ function ann($n){
     return $doneA;
 }
 
-print_r(john(11));
-print_r(ann(6))
+function sumJohn($n){
+    return array_sum(john($n));
+}
+
+function sumAnn($n){
+    return array_sum(ann($n));
+}
+
 ?>
