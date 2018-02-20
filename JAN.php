5993ba137790431261fe15897db8ae0160c7b9d6
Maks Ignatchenko <foecunditate@gmail.com>
2018-02-20 09:57:33 +0200

    two function completed

--- JAN.php
+++ JAN.php
@@ -1,13 +1,4 @@
 <?php
-function ann($n){
-    $done = [];
-    for ($i = 0; $i <$n; $i++){
-        if ($i == 0) $done[$i] = 1;
-            else $done[$i] = $i - $done[$i - 1];
-    }
-    return $done;
-}
-
 function john($n){
     $doneA[0] = 1;
     $doneJ[0] = 0;
@@ -18,4 +9,15 @@ function john($n){
     return $doneJ;
 }
 
+function ann($n){
+    $doneA[0] = 1;
+    $doneJ[0] = 0;
+    for ($i = 1; $i <= $n; $i++){
+        $doneJ[$i] = $i - $doneA[$doneJ{$i - 1}];
+        $doneA[$i] = $i - $doneJ[$doneA{$i - 1}];
+    }
+    return $doneA;
+}
+
 print_r(john(6));
+?>
